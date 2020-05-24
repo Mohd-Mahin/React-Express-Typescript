4587e5a41baa1ae41f840e71a976dacb2ce26e31
Mohd-Mahin <mahinit3016@gmail.com>
2020-05-24 17:41:17 +0530

    file removed

--- app.ts
+++ app.ts
@@ -5,31 +5,23 @@ import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 14000;
-const clientBuild = path.join(__dirname, "client");
+const clientBuild = path.join(__dirname, "client", "build");
 const isProd = process.env.NODE_ENV === "production";
-
-// function devRedirect(req, res, next) {
-//   if (process.env.NODE_ENV !== "production") {
-//     res.redirect(`https://localhost:${PORT}`);
-//   } else next();
-// }
 
 function notFound(req, res: Response, next) {
   res.status(404).send("Not Found !!!");
   next();
 }
 
-app.use("/client", express.static(clientBuild));
-
+app.use(express.static(clientBuild));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", shopRouter);
 
-console.log(path.join(clientBuild, "build", "index.html"));
-
-// if (isProd)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(clientBuild, "build", "index.html"));
-});
+if (isProd)
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(clientBuild, "index.html"));
+  });
+else app.get("*", notFound);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
