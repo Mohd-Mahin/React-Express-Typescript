30ac51aa179f80d639a1710f5a47ba8fb5c5bcdd
Mohd-Mahin <mahinit3016@gmail.com>
2020-05-24 17:45:21 +0530

    minor changes

--- app.ts
+++ app.ts
@@ -14,6 +14,7 @@ function notFound(req, res: Response, next) {
 }
 
 app.use(express.static(clientBuild));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", shopRouter);
