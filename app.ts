dc87cad614c15dc5d6374e46468e44d051a48cda
Mohd-Mahin <mahinit3016@gmail.com>
2020-05-24 07:07:06 +0530

    test case 1

--- app.ts
+++ app.ts
@@ -1,17 +1,38 @@
-import express from "express";
-import dotenv from "dotenv";
+import express, { Response } from "express";
 import bodyParser = require("body-parser");
 import shopRouter from "./routes/shop";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 14000;
+// const distFolder = "./client/dist";
+const clientBuild = path.join(__dirname, "client", "build");
+const isProd = process.env.NODE_ENV === "production";
 
-dotenv.config();
+// function devRedirect(req, res, next) {
+//   if (process.env.NODE_ENV !== "production") {
+//     res.redirect(`https://localhost:${PORT}`);
+//   } else next();
+// }
 
-app.set("view engine", "ejs");
+function notFound(req, res: Response, next) {
+  res.status(404).send("Not Found !!!");
+  next();
+}
+
+app.use("/client/dist", express.static(clientBuild), notFound);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", shopRouter);
+
+console.log(path.join(clientBuild, "index.html"));
+
+// if (isProd)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientBuild, "index.html"));
+});
+// else app.get("*", notFound);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div>React App Running with Typescript !!!</div>;
+}
+
+export default App;
