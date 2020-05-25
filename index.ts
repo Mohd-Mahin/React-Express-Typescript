399bce3cb8af3246efc60bc1865b287d76a62b8a
Mohd-Mahin <mahinit3016@gmail.com>
2020-05-26 01:29:07 +0530

    final commit

--- index.ts
+++ index.ts
@@ -6,24 +6,18 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 14000;
 const isProd = process.env.NODE_ENV === "production";
+const distFolder = "../client/build";
 
 function notFound(req, res: Response, next) {
   res.status(404).send("Not Found !!!");
   next();
 }
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", shopRouter);
 
-if (isProd)
-  // app.get("*", (req, res) => {
-  // res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-  // app.use(express.static(path.join(__dirname, "..", "public")));
-  app.use(express.static(path.join(__dirname, "..", "public")));
-// });
+if (isProd) app.use(express.static(path.join(__dirname, distFolder)));
 else app.get("*", notFound);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
