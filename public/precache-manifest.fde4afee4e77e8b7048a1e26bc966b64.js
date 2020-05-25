8737e10b7361e3eba3c55e7d66030eaa01fc3084
Mohd-Mahin <mahinit3016@gmail.com>
2020-05-26 00:06:17 +0530

    dfdf

---
+++ public/precache-manifest.fde4afee4e77e8b7048a1e26bc966b64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bcbda18cc7338a4a269e755045530d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa9aeed462219f26faf",
+    "url": "/static/js/2.bed78e95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bed78e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62274f39d92d6fe0d8c7",
+    "url": "/static/js/main.a6a5eaeb.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
