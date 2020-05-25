46d9318b3fdd7c697ecc7ae132ee0234ead746df
Mohd-Mahin <mahinit3016@gmail.com>
2020-05-25 23:00:39 +0530

    f

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ app.use("/api", shopRouter);
 
 if (isProd)
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 else app.get("*", notFound);
 
