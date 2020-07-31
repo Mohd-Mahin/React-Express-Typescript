d9c8031e679f319d782c87a49218a297c757a4f5
Mohd-Mahin <mahinit3016@gmail.com>
2020-07-31 16:48:25 +0530

    testing deploy

--- index.ts
+++ index.ts
@@ -25,26 +25,3 @@ async function main() {
 }
 
 main().catch((err) => console.error("app.init.failed", err));
-
-// import express, { Response } from "express";
-// import bodyParser = require("body-parser");
-// import path from "path";
-
-// const app = express();
-// const PORT = process.env.PORT || 14000;
-// const isProd = process.env.NODE_ENV === "production";
-// const distFolder = "../client/build";
-
-// function notFound(req, res: Response, next) {
-//   res.status(404).send("Not Found !!!");
-//   next();
-// }
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use("/api", shopRouter);
-
-// if (isProd) app.use(express.static(path.join(__dirname, distFolder)));
-// else app.get("*", notFound);
-
-// app.listen(PORT, () => console.log(`listening on port ${PORT}`));
